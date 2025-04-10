6094c07fd3a0496af41c6b8b668578a55aba4e05
opiyobrian2017 <opiyobrian2017@gmail.com>
2025-04-10 20:15:29 +0300

    updatind answers.py, adding error to display if the file cannot be opened

--- answers.py
+++ answers.py
@@ -7,8 +7,14 @@ with open("answers.txt", "w") as file:
 # Question 2
 
 try:
+    # open file with the name entered by the user
     with open(input("Enter the filename: "), "r") as file:
         data = file.read()
         print(data)
+#display error if file is not found
 except FileNotFoundError:
     print("File not found")
+
+#display error if file is not readable  
+except PermissionError:
+    print("file cant be read")
